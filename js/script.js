3f4fa78160b4a32b47729d7b6e89f24228f7edf6
Fillefisken7 <filip.skehammar@elev.ga.ntig.se>
2025-03-07 14:23:13 +0100

    nr 3
    massa saker har hänt

--- js/script.js
+++ js/script.js
@@ -12,5 +12,26 @@ function showMenu(){
  menu.classList.toggle("show")
 
  let layer = document.querySelector(".fade-layer");
-layer.classList.toggle("visible")
+ layer.classList.toggle("visible")
 }
+
+
+ document.addEventListener('DOMContentLoaded', function () {
+    const heroSection = document.querySelector('.gif')
+    const videoElement = document.qurentElemnt('.video')
+
+    videoElement.className = 'hero-video';
+    videoElement.autoplay = true;
+    videoElement.muted = true; 
+    videoElement. loop = true;
+    videoElement.playInline = true; 
+
+
+    const sourceElement = document.createElement('source');
+    sourceElemen.src = '/video/Film biograd.mp4';
+    sourceElement.type = 'video/mp4';
+
+    videoElement.appendChild(sourceElement);
+
+    heroSection.insertBefore(videoElement, heroSection.firstChild)
+});
